1a65b8d7ce8eb04179d033df6f9351f2c0b4345e
chrzyki <christoph@functor-argument.net>
2019-10-29 13:17:19 +0100

    Static sources, removed EvoBib.

--- lexibank_logos.py
+++ lexibank_logos.py
@@ -3,22 +3,12 @@ from pathlib import Path
 import lingpy
 from clldutils.misc import slug
 from pylexibank.dataset import Dataset as BaseDataset
-from pylexibank.util import progressbar, getEvoBibAsBibtex
+from pylexibank.util import progressbar
 
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
     id = "logos"
-
-    def cmd_download(self, args):
-        src = {}
-        for k in ["CLICS-1.0.0", "Wold2009", "Logos2008", "Key2007", "Saxena2013"]:
-            src[k] = getEvoBibAsBibtex(k)
-            # TODO: As far as I can tell, we have no obvious way of passing _check_id=False to
-            # add_sources() in cldfbench. Will investigate. This fixes the 'invalid' bib key.
-            if k == "CLICS-1.0.0":
-                src[k] = src[k].replace("CLICS-1.0.0", "CLICS1")
-        self.raw_dir.write("sources.bib", "\n\n".join(src.values()))
 
     def cmd_makecldf(self, args):
         wl = lingpy.Wordlist((self.raw_dir / "D_old-clics.tsv").as_posix())
